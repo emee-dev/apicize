2481fe399fe63eb0927259659f31efa2f6c1838c
Jason Terando <jasonterando@yaho.com>
2024-10-27 16:45:33 -0500

    Added lib-rust publish info

--- @apicize/toolkit/src/controls/editor-title.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, Typography } from "@mui/material";
-
-export function EditorTitle(props: { name: string, icon: JSX.Element }) {
-    return (
-        <Typography variant='h1' className='editor-title' aria-label={props.name} component='div' sx={{margin: 0}}>
-            {props.icon}<Box className='text'>{props.name?.length ?? 0 > 0 ? props.name : '(Unnamed)'}</Box>
-        </Typography>
-    )
-}
